ea6b5459ed98a62e7f0b24bae0aac7fb4fb45f23
Karol <naerriel@gmail.com>
2018-04-07 21:21:18 +0200

    Valid loading data.json

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 App = {};
-App.sortedData = MyData;
-App.filteredData = MyData;
 
 document.addEventListener('DOMContentLoaded', function() {
+  fetch("https://naerriel.github.io/XSolve-Task/data.json", function(text){
+    MyData = JSON.parse(text);
+    App.sortedData = MyData;
+    App.filteredData = MyData;
+  });
   App.view.render(MyData);
   App.view.handlePaginationButtons();
   App.view.handleFilteringInput();
