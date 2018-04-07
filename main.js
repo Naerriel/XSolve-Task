ab3e9b9538849470f56d1a7ec62e21a290423ae3
Karol <naerriel@gmail.com>
2018-04-07 21:35:58 +0200

    Fix loading file 2

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 App = {};
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetch("https://naerriel.github.io/XSolve-Task/data.json", function(text){
-    console.log(text);
-    MyData = JSON.parse(text);
-    App.sortedData = MyData;
-    App.filteredData = MyData;
-  });
-  App.view.render(MyData);
-  App.view.handlePaginationButtons();
-  App.view.handleFilteringInput();
-  App.view.handleSortingByColumns();
+  fetch("https://naerriel.github.io/XSolve-Task/data.json")
+    .then(response => response.json())
+    .then(json => {
+      MyData = json;
+      App.filteredData = MyData;
+      App.sortedData = MyData;
+      App.view.render(MyData);
+      App.view.handlePaginationButtons();
+      App.view.handleFilteringInput();
+      App.view.handleSortingByColumns();
+    });
 });
